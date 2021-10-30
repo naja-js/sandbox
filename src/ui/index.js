ea911f28b3d13fafdf8f31f57d289a3c04e1a3cd
Jiří Pudil <me@jiripudil.cz>
2021-10-30 14:52:57 +0200

    chore: update dependencies

--- src/ui/index.js
+++ src/ui/index.js
@@ -2,4 +2,4 @@ import naja from 'naja';
 import netteForms from 'nette-forms';
 
 naja.formsHandler.netteForms = netteForms;
-document.addEventListener('DOMContentLoaded', () => naja.initialize());
+naja.initialize();
